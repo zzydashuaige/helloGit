19c4370d6fb1515798e86e8f36c2cea16023502c
tom_glb <goodMorning_glb@qq.com>
2021-01-07 21:42:34 +0800

    克隆的添加

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -9,6 +9,7 @@ public class HelloGit {
         System.out.println("主干添加");
         System.out.println("分支添加");
           System.out.println("在线更新");
+          System.out.println("克隆下来的项目添加");
 
     }
 
