833c0be37f59ef734c280df6160b2f33f2300a0a
tom_glb <goodMorning_glb@qq.com>
2021-01-07 20:14:51 +0800

    更新1

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -4,6 +4,7 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("更新1");
     }
 
 }
